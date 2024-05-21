3e6898fd34f1429c654e6793a630f190d0e83a3a
Akinrotimi Dominion <134118389+DominionAkinrotimi@users.noreply.github.com>
2024-05-21 14:29:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import joblib
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, MultiLabelBinarizer
 
 # Load the trained model and encoders
